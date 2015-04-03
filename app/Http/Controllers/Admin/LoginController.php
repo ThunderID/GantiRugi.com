cb2ab830f80ca3f8e5134fc68085aeb2e17cdda4
erickmo <erick.mo@vortege.com>
2015-04-03 22:46:26 +0700

    Create Admin Login

--- app/Http/Controllers/Admin/LoginController.php
+++ app/Http/Controllers/Admin/LoginController.php
@@ -1,7 +1,7 @@
 <?php namespace App\Http\Controllers\Admin;
 
-use Input, Auth, \Illuminate\Support\MessageBag;
-use \App\API;
+use Input, Auth, Session, \Illuminate\Support\MessageBag;
+use \App\APIConnector\API;
 
 class LoginController extends BaseController {
 
@@ -13,8 +13,27 @@ class LoginController extends BaseController {
 	 **/
 	function getLogin()
 	{
-		$this->layout->content = view('admin.login.form');
-		return $this->layout;
+		// page layout
+		$this->html->section_body 				= view('admin.pages.login.index');
+
+		$this->html->section_body->area_1		= view('admin.cards.common.alerts');
+		
+		$this->html->section_body->area_2 		= view('admin.cards.login.form')->with('card_class', 'style-transparent')
+																				->with('title', 'Sign In')
+																				->with('form_href', route('admin.login.post'))
+																				->with('field_username', 'email')
+																				->with('field_remember_me', 'remember');
+
+		$this->html->section_body->area_3 		= view('admin.cards.login.forgot_password')->with('card_class', 'style-transparent')
+																				->with('title', 'Sign In')
+																				;
+		// $this->html->page_layout->left_content 	= view('admin.html_blank.pages.login.form');
+		// $this->html->page_layout->right_content = view('admin.html_blank.pages.login.description');
+
+
+
+		// return view
+		return $this->html;
 	}
 
 	/**
@@ -25,17 +44,14 @@ class LoginController extends BaseController {
 	 **/
 	function postLogin()
 	{
-		$results = json_decode(API::authenticate(Input::get('email'), Input::get('password')));
-		dd($results);
-		if ($results->status == 'success')
+		$credential = Input::only('email', 'password');
+		if (Auth::attempt($credential))
 		{
-			return 1;
-			// return redirect()->route('admin.dashboard.overview');
+			return redirect()->route('admin.dashboard.overview');
 		}
 		else
 		{
-			return 0;
-			// return redirect()->back()->withErrors(new MessageBag(['Invalid Username & Password']));
+			return redirect()->back()->withErrors(new MessageBag(['Invalid Username & Password']));
 		}
 	}
 
