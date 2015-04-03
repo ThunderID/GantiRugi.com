5ecca1c34298eff4d84e3fabdfc967aaf6df2f86
erickmo <erick.mo@vortege.com>
2015-04-03 22:44:33 +0700

    Admin Routes

--- app/Http/Controllers/Admin/routes.php
+++ app/Http/Controllers/Admin/routes.php
@@ -3,14 +3,32 @@
 // ------------------------------------------------------------------------
 // ROUTES FOR ADMIN
 // ------------------------------------------------------------------------
-Route::group(['prefix' => 'admin', 'namespace' => 'App\Http\Controllers\Admin'], function() {
+Route::group(['prefix' => 'admin', 'before' => 'csrf'], function() {
 
-	Route::get('/', ['as' => 'admin.login', 'uses' => '\App\Http\Controllers\Admin\LoginController@getLogin']);
-	Route::post('/', ['as' => 'admin.login.post', 'uses' => '\App\Http\Controllers\Admin\LoginController@postLogin']);
-	Route::get('/logout', ['as' => 'admin.logout', 'uses' => '\App\Http\Controllers\Admin\LoginController@getLogout']);
+	$namespace = '\App\Http\Controllers\Admin\\';
 
-	Route::group([], function(){
-		
+	Route::get('/', ['as' => 'admin.login', 'uses' => $namespace . 'LoginController@getLogin']);
+	Route::post('/', ['as' => 'admin.login.post', 'uses' => $namespace . 'LoginController@postLogin']);
+	Route::get('/logout', ['as' => 'admin.logout', 'uses' => $namespace . 'LoginController@getLogout']);
+
+	Route::group([], function() use ($namespace) {
+
+		Route::controller('dashboard', $namespace . 'DashboardController', [
+											'getOverview' => 'admin.dashboard.overview'
+						]);
+
+		$controllers = [
+							'vendor'	=> 'VendorController',
+					   ];
+		foreach ($controllers as $k => $controller)
+		{
+		Route::controller($k, $namespace . $controller, [
+												'getIndex' 		=> 'admin.'.$k.'.index',
+												'getCreate' 	=> 'admin.'.$k.'.create',
+												'postStore' 	=> 'admin.'.$k.'.store',
+												'getShow' 		=> 'admin.'.$k.'.show',
+												'getDelete' 	=> 'admin.'.$k.'.delete'
+								]);
+		}
 	});
-
 });
